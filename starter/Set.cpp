a79ed7e0d575fc56af8147b4aa09a3cb9a7f5381
Julian <jgaray0817@gmail.com>
2017-02-12 17:41:48 -0500

    stuff

---
+++ starter/Set.cpp
@@ -0,0 +1,23 @@
+#include "Set.h"
+
+	unsigned int Set::size() const {
+    return 0;
+  };
+	bool Set::empty() const {
+    return false;
+  };
+
+	bool Set::contains(const int& data) const {
+    return false;
+  };
+
+	bool Set::insert(const int& data) {
+    return false;
+  };
+	bool Set::remove(const int& data) {
+    return false;
+  };
+
+	void Set::clear() {
+  
+  };
